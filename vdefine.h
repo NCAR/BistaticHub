1aeffaaa138727a626c997581770120977d15149
Chris Burghart <burghart@ucar.edu>
2001-08-28 16:12:47 +0000

    Switch to C-style comment delimiters, since not all C compilers like "//" comments.

--- vdefine.h
+++ vdefine.h
@@ -1,5 +1,5 @@
-//
-// $Id: vdefine.h,v 1.1 2000/08/29 21:24:10 burghart Exp $
-// Placeholder header file, here only to make Mitch's globals.h happy!
-//
-typedef void SCALE;	// bogus
+/*
+ * $Id: vdefine.h,v 1.2 2001/08/28 16:12:47 burghart Exp $
+ * Placeholder header file, here only to make Mitch's globals.h happy!
+ */
+typedef void SCALE;	/* bogus */
