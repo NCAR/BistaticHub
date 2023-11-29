f74bb60c7c63fa34889a2c91efb4bbe778bfb831
Chris Burghart <burghart@ucar.edu>
2023-11-29 12:07:01 -0700

    Update to use Python 3 and current tkinter syntax

--- BistaticHub.py
+++ BistaticHub.py
@@ -1,91 +1,92 @@
 #!/usr/bin/env python
 import os
 import sys
-import regex
-import string
-#import select
+import re
+#import string
 import signal
 import fcntl
 import time
-import FCNTL
-from tkSimpleDialog import Dialog
-from Tkinter import *
+import tkinter
+from tkinter.simpledialog import Dialog
+from tkinter import Frame, Label, Button, Text, Scrollbar, Pack, IntVar, \
+                    Radiobutton, Entry, StringVar, Menu, Menubutton
 
 class BistaticHub( Frame ):
     def __init__( self, master=None ):
-	Frame.__init__( self, master )
+        Frame.__init__( self, master )
         self.debuglevel = 0
         self.ncpthresh = -1.0
+        self.winfo_toplevel().title("Bistatic Hub Control and Status")
         self.createWidgets()
-	Pack.config( self )
-	self.rcvrs = {}
+        Pack.config( self )
+        self.rcvrs = {}
+        
         signal.signal( signal.SIGCHLD, self.childDied )
         signal.signal( signal.SIGTERM, self.sigExit )
         signal.signal( signal.SIGINT, self.sigExit )
         signal.signal( signal.SIGSEGV, self.sigExit )
         signal.signal( signal.SIGQUIT, self.sigExit )
         self.startclient()
-	tkinter.createfilehandler( self.from_client.fileno(), READABLE,
-				   self.incoming )
+        self.tk.createfilehandler( self.from_client.fileno(), tkinter.READABLE, self.incoming )
 
     def createWidgets( self ):
-        #
-        # title
-        #
-	title = Label( self, text="Bistatic Hub Control and Status",
-                       relief=RIDGE, bg="lightblue" )
-        title.pack( side=TOP, fill=X )
         #
         # bottom buttons
         #
         buttonframe = Frame( self )
-        buttonframe.pack( side=BOTTOM, fill=X )
+        buttonframe.pack( side=tkinter.BOTTOM, fill=tkinter.X )
 
         self.recbutton = Button( buttonframe, text="Hub Recording is: unknown",
                                  command=self.toggleRecording )
-        self.recbutton.pack( side=LEFT )
+        self.recbutton.pack( side=tkinter.LEFT )
 
         self.debugbutton = Button( buttonframe, text="Debug Level: unknown",
                                    command=self.debugDialog )
-        self.debugbutton.pack( side=LEFT )
+        self.debugbutton.pack( side=tkinter.LEFT )
 
         self.ncpbutton = Button( buttonframe, text="NCP threshold: unknown",
-	                         command=self.ncpDialog )
-        self.ncpbutton.pack( side=LEFT )
+                             command=self.ncpDialog )
+        self.ncpbutton.pack( side=tkinter.LEFT )
 
         self.sendbutton = Button( buttonframe, text="Send Command",
                                   command=self.rcvrCmdDialog )
-        self.sendbutton.pack( side=LEFT )
+        self.sendbutton.pack( side=tkinter.LEFT )
 
         Button( buttonframe, text="Shut Down", command=self.done,
-                fg="red4" ).pack( side=RIGHT )
+                fg="red4" ).pack( side=tkinter.RIGHT )
 
         #
         # text area for general messages
         #
         textframe = Frame( self )
-        textframe.pack( side=BOTTOM, fill=BOTH )
+        textframe.pack( side=tkinter.BOTTOM, fill=tkinter.BOTH )
         self.textarea = Text( textframe )
-        self.textarea.pack( side=LEFT, fill=BOTH )
+        self.textarea.pack( side=tkinter.LEFT, fill=tkinter.BOTH )
         scroll = Scrollbar( textframe )
         scroll.config( command=self.textarea.yview )
-        scroll.pack( side=RIGHT, fill=BOTH )
+        scroll.pack( side=tkinter.RIGHT, fill=tkinter.BOTH )
         self.textarea.config( yscrollcommand=scroll.set, height=15, width=80,
-                              wrap=CHAR )
+                              wrap=tkinter.CHAR )
         self.textarea.column = 0
 
     def incoming( self, fd, unknown ):
-        line = ""
+        # Read byte-by-byte until we hit a newline
+        byteline = b""  # empty byte string
         while 1:
             try:
                 c = self.from_client.read( 1 )
                 if (not c):
                     break
-                line = line + c
-                if (c == "\n"):
+
+                byteline += c
+                if (c == b"\n"):
                     break
             except IOError:
                 break
+
+        # Now convert the line of text bytes into 'normal' Python string
+        # representation to make further parsing simpler
+        line = byteline.decode()
 
         #
         # Read stuff from BistaticHub, with special handling for certain
@@ -94,25 +95,29 @@ class BistaticHub( Frame ):
         if (len( line ) == 0):
             return
         #
-        # Look for the strings we handle specially
+        # Look for the leading strings we handle specially
         #
         specials = [
             ("receiver:", self.handleRcvr),
             ("recording:", self.handleRecStatus),
             ("debuglevel:", self.handleDebugLevel),
-	    ("ncpthresh:", self.handleNCPThresh),
+            ("ncpthresh:", self.handleNCPThresh),
             ("status:", self.handleStatus),
             ("syncinfo:", self.handleSyncInfo)
         ]
 
         for string, handler in specials:
-            start = regex.search( string, line )
-            if (start != -1):
+            match = re.search( string, line )
+            if (match):
+                start = match.start()
                 if (start != 0):
-                    self.log( line[:start] )
+                    self.log( line[:start] )    # log any extraneous stuff preceding the special
                 handler( line[start:] )
+
+                # Return after we handle a match
                 return
 
+        # If the line wasn't handled above, just log it as an informational message
         self.log( line )
 
     def handleRcvr( self, line ):
@@ -120,42 +125,45 @@ class BistaticHub( Frame ):
         # Deal with receiver info line:
         #   receiver: <rnum> <rname> <enabled> [<extra info>]
         #
-	r = regex.compile(
-            'receiver: *\([0-9]+\) +\([^ ]+\) +\([0-9]+\) \(.*\)' )
-	r.match( line )
-	rnum = eval( r.group( 1 ) )
- 	name = r.group( 2 )
- 	enabled = eval( r.group( 3 ) )
-        extra = r.group( 4 )
+        match = re.search( 'receiver: *([0-9]+) +([^ ]+) +([0-9]+) (.*)', line )
+        if (match == None):
+            self.log( "Bad 'receiver' line: " + line )
+            return
+
+        rnum = match.group(1)
+        name = match.group(2)
+        enabled = match.group(3)
+        extra = match.group(4)
+
         #
         # If we already have this receiver in our list, update its state,
         # otherwise create it
         #
-	try:
-	    self.rcvrs[name].updateEnableState( enabled )
-	except KeyError:
-	    self.rcvrs[name] = Receiver( name, rnum, enabled,
-                                         self.sendToClient, self )
-	    self.pack()
-        #
-        # If we got extra information, put it into the receiver status
-        # in red.  (We also blank the sync info for the receiver)
-        #
-        self.rcvrs[name].updateStatus( extra, "red2" )
-        self.rcvrs[name].updateSyncInfo( "" )
+        try:
+            self.rcvrs[name].updateEnableState( enabled )
+        except KeyError:
+            self.rcvrs[name] = Receiver( name, rnum, enabled,
+                                             self.sendToClient, self )
+            self.pack()
+            #
+            # If we got extra information, put it into the receiver status
+            # in red.  (We also blank the sync info for the receiver)
+            #
+            self.rcvrs[name].updateStatus( extra, "red2" )
+            self.rcvrs[name].updateSyncInfo( "" )
 
     def handleStatus( self, line ):
         #
         # Deal with status line:
         #   status: <rname> <status stuff...>
         #
-        fields = string.split( line )
+        fields = line.split()
         rname = fields[1]
-        status = string.join( fields[2:] )
+        status = ' '.join(fields[2:])
 
-	try:
-	    self.rcvrs[rname].updateStatus( status )
-	except KeyError:
+        try:
+            self.rcvrs[rname].updateStatus( status )
+        except KeyError:
             self.log( "Attempt to update status for unknown receiver " +
                       rname + "\n" )
 
@@ -164,15 +172,15 @@ class BistaticHub( Frame ):
         # Deal with syncinfo line:
         #   syncinfo: <rname> p <p0> ... <p7> v <v0> ... <v7>
         #
-        fields = string.split( line )
+        fields = line.split()
         rname = fields[1]
         p = fields[3:11]
         v = fields[12:20]
-        line = "P " + string.join( p ) + "    V " + string.join( v )
+        line = "P " + " ".join(p) + "    V " + " ".join(v)
         self.rcvrs[rname].updateSyncInfo( line )
 
     def handleRecStatus( self, line ):
-        self.recording = eval( string.split( line )[1] )
+        self.recording = eval( line.split()[1] )
         
         if (self.recording):
             statusString = "ON"
@@ -186,7 +194,7 @@ class BistaticHub( Frame ):
     def debugDialog( self ):
         newlevel = DebugLevelDialog( self, self.debuglevel ).result
         if (newlevel != None):
-            self.sendToClient( "debuglevel " + `newlevel` )
+            self.sendToClient( "debuglevel " + repr(newlevel) )
 
     def ncpDialog( self ):
         newthresh = NCPThreshDialog( self, self.ncpthresh ).result
@@ -204,52 +212,52 @@ class BistaticHub( Frame ):
         else:
             rnum = self.rcvrs[recipient].rcvrNum
             
-        self.sendToClient( "rcvrcommand " + cmd + ' ' + `rnum` )
+        self.sendToClient( "rcvrcommand " + cmd + ' ' + repr(rnum) )
 
     def handleDebugLevel( self, line ):
-        self.debuglevel = eval( string.split( line )[1] )
-        self.debugbutton.config( text="Debug Level: " + `self.debuglevel` )
+        self.debuglevel = eval( line.split()[1] )
+        self.debugbutton.config( text="Debug Level: " + repr(self.debuglevel) )
 
     def handleNCPThresh( self, line ):
-        self.ncpthresh = eval( string.split( line )[1] )
-        self.ncpbutton.config( text="NCP Threshold: " + `self.ncpthresh` )
+        self.ncpthresh = eval( line.split()[1] )
+        self.ncpbutton.config( text="NCP Threshold: " + repr(self.ncpthresh) )
 
     def log( self, text ):
         #
         # force line breaking if necessary
         #
-        width = eval( self.textarea.cget( "width" ) )
+        width = self.textarea.cget( "width" )
 
         nleft = width - self.textarea.column
         while (len( text ) > nleft):
             partial = text[:nleft]
-            self.textarea.insert( END, partial + "\n" )
+            self.textarea.insert( tkinter.END, partial + "\n" )
             text = text[nleft-1:]
             self.textarea.column = 0
             nleft = width
 
-        self.textarea.insert( END, text )
+        self.textarea.insert( tkinter.END, text )
         if (text[-1] == '\n'):
             self.textarea.column = 0
         else:
-            lastline = string.splitfields( text, "\n" )[-1]
+            lastline = '\n'.split(text)[-1]
             self.textarea.column = self.textarea.column + len( lastline )
         #
         # Just eval the line.column index as a float, then convert it
         # to an int to get our line count
         #
-        nlines = int( eval( self.textarea.index( END ) ) )
+        nlines = int( eval( self.textarea.index( tkinter.END ) ) )
         #
         # Keep our size down to 200 lines or fewer
         #
         nkill = nlines - 200
         if (nkill > 0):
-            self.textarea.delete( "0.0", `nkill` + ".0" )
+            self.textarea.delete( "0.0", repr(nkill) + ".0" )
         #
         # Scroll so that we keep the new stuff in the window
         #
-        totallines = int( eval( self.textarea.index( END ) ) )
-        winheight = eval( self.textarea.cget( "height" ) )
+        totallines = int( eval( self.textarea.index( tkinter.END ) ) )
+        winheight = self.textarea.cget( "height" )
         top = 1.0 - (float( winheight - 4 ) / totallines)
         if top < 0:
             top = 0
@@ -259,7 +267,7 @@ class BistaticHub( Frame ):
         fname = "BistaticHub.log"
         try:
             f = open( fname, "w" )
-            f.write( self.textarea.get( 1.0, END ) )
+            f.write( self.textarea.get( 1.0, tkinter.END ) )
             f.close()
             sys.stderr.write("Log information dumped to " + fname + "\n" )
         except:
@@ -273,13 +281,12 @@ class BistaticHub( Frame ):
         if (os.fork()):
             os.close( pipe_a[0] )
             os.close( pipe_b[1] )
-            self.to_client = os.fdopen( pipe_a[1], 'w', 0 )
-            self.from_client = os.fdopen( pipe_b[0], 'r', 0 )
+            self.to_client = os.fdopen( pipe_a[1], 'wb', 0 )
+            self.from_client = os.fdopen( pipe_b[0], 'rb', 0 )
             #
             # set up our input file for non-blocking reads
             #
-            fcntl.fcntl( self.from_client.fileno(), FCNTL.F_SETFL,
-                         FCNTL.O_NDELAY )
+            fcntl.fcntl( self.from_client.fileno(), fcntl.F_SETFL, os.O_NDELAY )
         else:
             os.close( pipe_a[1] )
             os.close( pipe_b[0] )
@@ -295,8 +302,10 @@ class BistaticHub( Frame ):
             #
             sys.exit( 1 )
 
+    # Send the given string command to our client as 8-bit characters
     def sendToClient( self, cmd ):
-        self.to_client.write( cmd + "\n" )
+        cmdbytes = (cmd + "\n").encode("utf-8")
+        self.to_client.write( cmdbytes )
 
     def toggleRecording( self ):
         if self.recording:
@@ -313,8 +322,8 @@ class BistaticHub( Frame ):
         sys.exit( 0 )
 
     def sigExit( self, signum, stack ):
-        sys.stderr.write( "Exiting on signal " + `signum` )
-        self.log( "Exiting on signal " + `signum` + "\n" )
+        sys.stderr.write( "Exiting on signal " + repr(signum) )
+        self.log( "Exiting on signal " + repr(signum) + "\n" )
         self.dumplog()
         self.haltChild()
         sys.exit( 1 )
@@ -334,11 +343,11 @@ class DebugLevelDialog( Dialog ):
     def __init__( self, master, currentlevel ):
         self.newlevel = IntVar()
         self.newlevel.set( currentlevel )
-	Dialog.__init__( self, master )
+        Dialog.__init__( self, master )
         
     def body( self, master ):
         for i in range(4):
-            Radiobutton( master, text=`i`, variable=self.newlevel,
+            Radiobutton( master, text=repr(i), variable=self.newlevel,
                          value=i ).grid( row=0, column=i )
 
     def apply( self ):
@@ -348,12 +357,12 @@ class DebugLevelDialog( Dialog ):
 class NCPThreshDialog( Dialog ):
     def __init__( self, master, currentthresh ):
         self.currentval = currentthresh
-	Dialog.__init__( self, master )
+        Dialog.__init__( self, master )
         
     def body( self, master ):
         Label( master, text="NCP threshold:" ).grid( row=0, column=0 )
         self.entry = Entry( master )
-	self.entry.insert( 0, self.currentval )
+        self.entry.insert( 0, self.currentval )
         self.entry.grid( row=0, column=1 )
         self.entry.config( width=5 )
 
@@ -366,7 +375,7 @@ class RcvrCmdDialog( Dialog ):
         self.rcvrs = rcvrs
         self.towhom = StringVar()
         self.towhom.set( "All" )
-	Dialog.__init__( self, master )
+        Dialog.__init__( self, master )
 
     def body( self, master ):
         Label( master, text="Send:" ).grid( row=0, column=0 )
@@ -375,13 +384,13 @@ class RcvrCmdDialog( Dialog ):
         self.entry.config( width=5 )
         Label( master, text="to" ).grid( row=0, column=2 )
         self.mb = Menubutton( master, text=self.towhom.get(), borderwidth=2,
-                         relief=RAISED )
+                         relief=tkinter.RAISED )
         self.mb.grid( row=0, column=3 )
         menu = Menu( self.mb )
         menu.add_radiobutton( label="All", variable=self.towhom, value="All",
                               indicatoron=0, command=self.updateMenuButton )
 
-        for r in self.rcvrs.keys():
+        for r in list(self.rcvrs.keys()):
             menu.add_radiobutton( label=r, variable=self.towhom,
                                   value=r, indicatoron=0,
                                   command=self.updateMenuButton )
@@ -396,45 +405,45 @@ class RcvrCmdDialog( Dialog ):
 
 class Receiver( Frame ):
     def __init__( self, name, rnum, enabled, sender, master=None ):
-	Frame.__init__( self, master, relief=SUNKEN, borderwidth=2 )
-	self.name = name
-	self.pack( side=TOP, fill=X )
-	self.rcvrNum = rnum
-	self.createWidgets()
+        Frame.__init__( self, master, relief=tkinter.SUNKEN, borderwidth=2 )
+        self.name = name
+        self.pack( side=tkinter.TOP, fill=tkinter.X )
+        self.rcvrNum = rnum
+        self.createWidgets()
         self.sendToClient = sender
         self.updateEnableState( enabled )
 
     def updateEnableState( self, enabled ):
-	self.enabled = enabled
-	if self.enabled:
+        self.enabled = enabled
+        if self.enabled:
             self.label.config( fg="black" )
-	else:
-	    self.label.config( fg="gray60" )
+        else:
+            self.label.config( fg="gray60" )
             self.status.config( text="" )
             self.sync.config( text="" )
 
     def updateStatus( self, status, color="black" ):
-	self.status.config( text=status, fg=color )
+        self.status.config( text=status, fg=color )
  
     def updateSyncInfo( self, info ):
-	self.sync.config( text=info )
+        self.sync.config( text=info )
 
     def createWidgets( self ):
-	self.label = Label( self, text=self.name, width=20, fg="red",
-                            anchor=E )
-	self.label.pack( side=LEFT )
-        Button( self, command=self.toggleEnable ).pack( side=LEFT )
-        self.status = Label( self, text="", anchor=W )
-        self.status.pack( side=TOP, fill=X )
-        self.sync = Label( self, text="", anchor=W )
-        self.sync.pack( side=BOTTOM, fill=X )
+        self.label = Label( self, text=self.name, width=20, fg="red",
+                                anchor=tkinter.E )
+        self.label.pack( side=tkinter.LEFT )
+        Button( self, command=self.toggleEnable ).pack( side=tkinter.LEFT )
+        self.status = Label( self, text="", anchor=tkinter.W )
+        self.status.pack( side=tkinter.TOP, fill=tkinter.X )
+        self.sync = Label( self, text="", anchor=tkinter.W )
+        self.sync.pack( side=tkinter.BOTTOM, fill=tkinter.X )
 
     def toggleEnable( self ):
-	if (self.enabled):
-            cmd = "disable " + `self.rcvrNum`
-	else:
-            cmd = "enable " + `self.rcvrNum`
-
+        if (self.enabled):
+            cmd = "disable " + repr(self.rcvrNum)
+        else:
+            cmd = "enable " + repr(self.rcvrNum)
+    
         self.sendToClient( cmd )
 
         
